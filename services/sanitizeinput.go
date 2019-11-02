edc7bd152ca3f5635b206e819853a1d63dca22d2
Ryan <rback@focusvision.com>
2019-11-01 23:01:32 -0700

    Add sanitizer and test it

---
+++ services/sanitizeinput.go
@@ -0,0 +1,22 @@
+package services
+
+import (
+	"fmt"
+
+	"github.com/microcosm-cc/bluemonday"
+)
+
+// SanitizeInterface makes an interface with a string value a string to be passed into SanitizeInput
+func SanitizeInterface(i interface{}) string {
+	s := fmt.Sprintf("%v", i)
+
+	return SanitizeInput(s)
+}
+
+// SanitizeInput will sanitize all harmful html
+// script, style, object, iframe, base,embed
+func SanitizeInput(s string) string {
+	p := bluemonday.UGCPolicy()
+
+	return p.Sanitize(s)
+}
---
+++ services/sanitizeinput_test.go
@@ -0,0 +1,51 @@
+package services
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestSanitizeInputScriptTagRemoveTag(t *testing.T) {
+	assert := assert.New(t)
+	htmlString := "<script>alert('hello world')</script>"
+	sanitizedString := SanitizeInput(htmlString)
+
+	assert.Equal("", sanitizedString)
+}
+
+func TestSanitizeInputScriptTagLeaveTextAroundIt(t *testing.T) {
+	assert := assert.New(t)
+	htmlString := "HELLO <script>alert('hello world')</script>WORLD"
+	sanitizedString := SanitizeInput(htmlString)
+
+	assert.Equal("HELLO WORLD", sanitizedString)
+}
+
+func TestSanitizeInputLeaveATag(t *testing.T) {
+	assert := assert.New(t)
+	htmlString := "<a href='example.com'>Click Here</a>"
+	sanitizedString := SanitizeInput(htmlString)
+
+	// Doesn't actually escape quotes, only here because it's a double quote within a double quote
+	assert.Equal("<a href=\"example.com\" rel=\"nofollow\">Click Here</a>", sanitizedString)
+}
+
+func TestSanitizeInputRemoveATagIfHrefIsJS(t *testing.T) {
+	assert := assert.New(t)
+	htmlString := "<a href=\"javascript:alert('XSS1')\" onmouseover=\"alert('XSS2')\">XSS<a>"
+	sanitizedString := SanitizeInput(htmlString)
+
+	assert.Equal("XSS", sanitizedString)
+}
+
+func TestSanitizeInterface(t *testing.T) {
+	assert := assert.New(t)
+	var payload = map[string]interface{}{
+		"html": "<a href=\"javascript:alert('XSS1')\" onmouseover=\"alert('XSS2')\">XSS<a>",
+	}
+
+	sanitizedString := SanitizeInterface(payload["html"])
+
+	assert.Equal("XSS", sanitizedString)
+}
